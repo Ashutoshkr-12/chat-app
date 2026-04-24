bf5e5ff24fb0f42e4498f6828fa859891c737835
Ashutosh <ashutoshkr.8920@gmail.com>
2026-04-24 18:39:51 +0530

    refactor: enhance CORS configuration and streamline server startup

--- backend/index.js
+++ backend/index.js
@@ -12,24 +12,38 @@ import { app,server } from './socket/socket.js';
 
 
 //const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;;
 // console.log(process.env.PORT);
 // console.log(process.env.MONGODB_URI);
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+];
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}))
+  origin: function(origin, callback){
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json({ limit: "10mb"}))
 app.use(express.urlencoded({extended: true , limit: "10mb"}))
 app.use(cookieParser())
 
-connectDB().then(()=>{
-    server.listen(PORT,()=>{
-        console.log(`backend is running at PORT: http://localhost:${PORT}`)
-    })
-})
+connectDB()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection failed:", err);
+    process.exit(1);
+  });
 
 app.get("/", (req, res) => {
   res.send("Backend is running...");
